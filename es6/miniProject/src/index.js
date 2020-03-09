a1375cf44e694801bae8324e0528492c88e5be4c
Areum Yang <s2018s10@e-mirim.hs.kr>
2020-03-10 02:35:30 +0900

    ◾ 완강!

--- es6/miniProject/src/index.js
+++ es6/miniProject/src/index.js
@@ -1,10 +1,3 @@
 import blog from './main.js';
 
 const myblog = new blog();
-
-// const myfun = () => {
-//     let name = "inflearn";
-//     console.log(`my name is ${name}`);
-// }
-
-// myfun();
